798b57b500673fa92758fb37718e60632dedf5d1
dnriski <nugrohoriski159@gmail.com>
2021-12-10 00:52:03 +0700

    error handling middleware

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+const { urlencoded } = require("express");
 const express = require(`express`);
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get(`/`, (req, res) => {
-  res.send("Hallo");
+  res.send("Hallosssas");
 });
 
-app.get(`/pandji`, (req, res) => {
-  res.send("Hallo pandji");
+//internal server
+app.get(`/contohError`, (req, res) => {
+  contohError;
+});
+app.use(function (err, req, res, next) {
+  res.status(500).json({
+    error: err.message,
+  });
+});
+
+//404 handler
+app.use(function (req, res, next) {
+  res.status(404).json({
+    errors: `You Typed Wrong`,
+  });
 });
 
 app.listen(8000, () => {
